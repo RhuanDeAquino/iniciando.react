7b003c2770d864ae20e1303c248c1e10d289d358
João Paulo <joaop.rosa01@gmail.com>
2019-11-29 12:37:06 -0300

    Show

---
+++ src/component/semaforo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../App.css';
+
+class semaforo extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          semaforo: 'semaforo apagado'
+        };
+    }
+    
+      ficaramarelo = () => {
+        this.setState({
+            semaforo: 'Amarelo', 
+        })
+      }
+
+        ficarverde = () => {
+            this.setState({
+                semaforo: 'Verde'
+            })
+        }
+
+        ficarvermelho = () => {
+            this.setState({
+                semaforo: 'Vermelho'
+            })
+        }
+      render() {
+      return (
+        <div className="App" style={'backgroundColor:red'}>
+            <p>{this.state.semaforo}</p>
+            <button onClick={this.ficarvermelho}>Vermelho</button>
+            <button onClick={this.ficaramarelo}>Amarelo</button>
+            <button onClick={this.ficarverde}>Verde</button>   
+        </div>
+      );
+    }
+}
+export default semaforo;
